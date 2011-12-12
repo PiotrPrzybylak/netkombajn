c019441243cff21d76783181120fe9a0fd860c59
krzysiek <430616+krzysztof-jelski@users.noreply.github.com>
2011-12-12 17:45:30 +0100

    Revert a bit to get ready for the session.

--- netkombajn/sklep3-common-web/src/test/java/pl/netolution/sklep3/web/controller/ProductDetailsControllerSpringTest.java
+++ netkombajn/sklep3-common-web/src/test/java/pl/netolution/sklep3/web/controller/ProductDetailsControllerSpringTest.java
@@ -1,7 +1,6 @@
 package pl.netolution.sklep3.web.controller;
 
 import static junit.framework.Assert.*;
-import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
 import org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter;
 
-import pl.netolution.sklep3.dao.ProductDao;
-import pl.netolution.sklep3.dao.hibernate.ProductRatingDao;
-import pl.netolution.sklep3.domain.Product;
-import pl.netolution.sklep3.domain.product.opinions.ProductRatings;
-import pl.netolution.sklep3.domain.product.opinions.SingleRating;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/applicationContext.xml", "/exception-context.xml", "/sessionContext.xml",
 		"/common-web-servlet.xml", "/beans.xml", "/test-objects.xml" })
@@ -30,28 +23,9 @@ public class ProductDetailsControllerSpringTest {
 	private SimpleControllerHandlerAdapter adapter;
 	@Autowired
 	private BeanNameUrlHandlerMapping beanNameUrlHandlerMapping;
-	@Autowired
-	private ProductDao productDao;
-	@Autowired
-	private ProductRatingDao productRatingDao;
 
 	@Test
 	public void shouldGetRatingTogetherWithProduct() throws Exception {
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		Product product = new Product();
-		product.setVisible(true);
-		when(productDao.findById(1L)).thenReturn(product);
-		ProductRatings ratings = ProductRatings.fromList(5);
-		when(productRatingDao.findByProductId(1L)).thenReturn(ratings);
-
-		request.setRequestURI("/product.html");
-		request.setParameter("productId", "1");
-
-		ModelAndView modelAndView = execute(request, new MockHttpServletResponse());
-
-		assertSame(product, modelAndView.getModel().get("product"));
-		assertEquals(new SingleRating().of(5),
-				((ProductRatings) modelAndView.getModel().get("ratings")).getAverageRating());
 	}
 
 	private ModelAndView execute(MockHttpServletRequest request, MockHttpServletResponse response) throws Exception {
