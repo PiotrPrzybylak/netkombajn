93e790b4e3967953da6178b8c6a47baa85bfd9f0
krzysiek <krzysiek@localhost>
2011-12-15 13:54:45 +0100

    After session.

--- netkombajn/sklep3-common-web/src/main/java/pl/netolution/sklep3/web/controller/ProductDetailsController.java
+++ netkombajn/sklep3-common-web/src/main/java/pl/netolution/sklep3/web/controller/ProductDetailsController.java
@@ -8,12 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
 
 import pl.netolution.sklep3.dao.ProductDao;
+import pl.netolution.sklep3.dao.hibernate.ProductRatingDao;
 import pl.netolution.sklep3.domain.Product;
 import pl.netolution.sklep3.exception.ProductNotAcessibleException;
 
 public class ProductDetailsController extends ParameterizableViewController {
 
 	private ProductDao productDao;
+
+	private ProductRatingDao productRatingDao;
 
 	@SuppressWarnings("unused")
 	private final static Logger log = Logger.getLogger(ProductDetailsController.class);
@@ -25,7 +28,9 @@ public class ProductDetailsController extends ParameterizableViewController {
 			throw new ProductNotAcessibleException();
 		}
 
-		return new ModelAndView(getViewName(), "product", product);
+		ModelAndView modelAndView = new ModelAndView(getViewName(), "product", product);
+		modelAndView.addObject("productRating", productRatingDao.findByProductId(getProductIdFrom(request)));
+		return modelAndView;
 	}
 
 	private long getProductIdFrom(HttpServletRequest request) {
@@ -34,6 +39,10 @@ public class ProductDetailsController extends ParameterizableViewController {
 
 	public void setProductDao(ProductDao productDao) {
 		this.productDao = productDao;
+	}
+
+	public void setProductRatingDao(ProductRatingDao productRatingDao) {
+		this.productRatingDao = productRatingDao;
 	}
 
 }
--- netkombajn/sklep3-common-web/src/test/java/pl/netolution/sklep3/web/controller/ProductDetailsControllerSpringTest.java
+++ netkombajn/sklep3-common-web/src/test/java/pl/netolution/sklep3/web/controller/ProductDetailsControllerSpringTest.java
@@ -1,6 +1,7 @@
 package pl.netolution.sklep3.web.controller;
 
 import static junit.framework.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,11 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
 import org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter;
 
+import pl.netolution.sklep3.dao.ProductDao;
+import pl.netolution.sklep3.dao.hibernate.ProductRatingDao;
+import pl.netolution.sklep3.domain.Product;
+import pl.netolution.sklep3.domain.product.opinions.ProductRatings;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/applicationContext.xml", "/exception-context.xml", "/sessionContext.xml",
 		"/common-web-servlet.xml", "/beans.xml", "/test-objects.xml" })
@@ -23,9 +29,25 @@ public class ProductDetailsControllerSpringTest {
 	private SimpleControllerHandlerAdapter adapter;
 	@Autowired
 	private BeanNameUrlHandlerMapping beanNameUrlHandlerMapping;
+	@Autowired
+	private ProductDao productDao;
+	@Autowired
+	private ProductRatingDao productRatingDao;
 
 	@Test
 	public void shouldGetRatingTogetherWithProduct() throws Exception {
+		Product product = new Product();
+		product.setVisible(true);
+		when(productDao.findById(11L)).thenReturn(product);
+		ProductRatings productRatings = ProductRatings.fromList(3);
+		when(productRatingDao.findByProductId(11L)).thenReturn(productRatings);
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setRequestURI("/product.html");
+		request.setParameter("productId", "11");
+
+		ModelAndView modelAndView = execute(request, new MockHttpServletResponse());
+
+		assertEquals(productRatings, modelAndView.getModel().get("productRating"));
 	}
 
 	private ModelAndView execute(MockHttpServletRequest request, MockHttpServletResponse response) throws Exception {
