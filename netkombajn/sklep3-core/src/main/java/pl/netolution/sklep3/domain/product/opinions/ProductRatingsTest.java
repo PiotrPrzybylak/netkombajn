0b9274ec5171a73a16032792a5843bf131a0d250
krzysiek <430616+krzysztof-jelski@users.noreply.github.com>
2011-12-15 14:41:11 +0100

    Make the test as I wanted it to look in the first place.

--- netkombajn/sklep3-core/src/main/java/pl/netolution/sklep3/domain/product/opinions/ProductRatingsTest.java
+++ netkombajn/sklep3-core/src/main/java/pl/netolution/sklep3/domain/product/opinions/ProductRatingsTest.java
@@ -15,13 +15,13 @@ public class ProductRatingsTest {
 
 	@Test
 	public void shouldGetAverage() throws Exception {
-		assertAverageRating(0, ProductRatings.fromList());
-		assertAverageRating(1, ProductRatings.fromList(1));
-		assertAverageRating(2, ProductRatings.fromList(1, 3));
+		assertAverageRating(0);
+		assertAverageRating(1, 1);
+		assertAverageRating(2, 1, 3);
 	}
 
-	private void assertAverageRating(int expectedAverage, ProductRatings ratings) {
-
+	private void assertAverageRating(int expectedAverage, Integer... ratingsAsIntegers) {
+		ProductRatings ratings = ProductRatings.fromList(ratingsAsIntegers);
 		assertThat(ratings.getAverageRating()).isEqualTo(SingleRating.of(expectedAverage));
 	}
 }
