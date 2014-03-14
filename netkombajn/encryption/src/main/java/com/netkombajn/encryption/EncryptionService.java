cad15a0497dda66918db1cc35b204627cb98cbcc
Piotr Przybylak <23518573+PiotrPrzybylak@users.noreply.github.com>
2014-03-14 15:40:54 +0000

    Refactoring of EncryptionService

--- netkombajn/encryption/src/main/java/com/netkombajn/encryption/EncryptionService.java
+++ netkombajn/encryption/src/main/java/com/netkombajn/encryption/EncryptionService.java
@@ -7,21 +7,22 @@ import java.security.NoSuchAlgorithmException;
 public class EncryptionService {
 
 	public String encode(String encode) {
-		MessageDigest messageDigest;
-		try {
-			messageDigest = MessageDigest.getInstance("MD5");
-			messageDigest.update(encode.getBytes());
-			byte[] result = messageDigest.digest();
-
-			return toText(result);
-
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
-		}
+		MessageDigest messageDigest = getMessageDigestForMD5();
+        messageDigest.update(encode.getBytes());
+        byte[] result = messageDigest.digest();
+        return toText(result);
 
 	}
 
-	private static String toText(byte[] result) {
+    private MessageDigest getMessageDigestForMD5() {
+        try {
+            return MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String toText(byte[] result) {
 		String string = "";
 		for (byte b : result) {
 			string += Integer.toString((b & 0xff) + 0x100, 16).substring(1);
